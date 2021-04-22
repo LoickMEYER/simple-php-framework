039871eba52c74a346ef539ceb156a773e826b5b
LoickMEYER <loick.meyer@gmail.com>
2021-04-22 21:52:50 +0200

    Hidfe warning and error when debug is false

--- index.php
+++ index.php
@@ -21,7 +21,11 @@ if ($config->getConfig('debug')) {
     ini_set('display_errors', 1);
     ini_set('display_startup_errors', 1);
     error_reporting(E_ALL);
+} else {
+    ini_set('display_errors', 0);
+    error_reporting(E_ALL ^ E_WARNING);
 }
+
 
 $request = new Request($_GET['url']);
 
